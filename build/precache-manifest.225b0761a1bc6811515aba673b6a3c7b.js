bc739d1adc3aff104aba51c38671c019c740ca8f
Chris Valmonte <jchrisvalmonte@gmail.com>
2020-03-29 20:45:48 -0700

    fix: Move all contents out of app dir and delete app dir

---
+++ build/precache-manifest.225b0761a1bc6811515aba673b6a3c7b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd333039acff425a84399e6aa4011e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "263891b14214a8109c14",
+    "url": "/static/css/2.23bc2e7b.chunk.css"
+  },
+  {
+    "revision": "5e9f56327f461a4d14e9",
+    "url": "/static/css/main.ff354200.chunk.css"
+  },
+  {
+    "revision": "263891b14214a8109c14",
+    "url": "/static/js/2.fd548f90.chunk.js"
+  },
+  {
+    "revision": "127fd8960f6efe83b0151074892b509b",
+    "url": "/static/js/2.fd548f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9f56327f461a4d14e9",
+    "url": "/static/js/main.3ab53e4e.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  },
+  {
+    "revision": "ff349f188240182bac2d80b523613381",
+    "url": "/static/media/biohazard.ff349f18.svg"
+  },
+  {
+    "revision": "f228fdd55ec7477271dee51823570651",
+    "url": "/static/media/img-not-available.f228fdd5.png"
+  },
+  {
+    "revision": "4d9c7c171d6ee75ffe10fb11e4f2c1da",
+    "url": "/static/media/logo.4d9c7c17.svg"
+  }
+]);
