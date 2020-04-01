d25126f53675f9bc6f5ef8da50a3323344b8c594
Chris Valmonte <jchrisvalmonte@gmail.com>
2020-04-01 11:33:24 -0700

    feat: Filter out news sources

--- src/services.js
+++ src/services.js
@@ -18,8 +18,57 @@ export const getUSStates = async () =>
   await axios.get('https://corona.lmao.ninja/states');
 
 // https://newsapi.org
-export const getNews = async ({ page = 1 }) =>
-  await axios.get('https://newsapi.org/v2/top-headlines', {
+export const getNews = async ({ page = 1 }) => {
+  const sources = [
+    'abc-news',
+    'abc-news-au',
+    'associated-press',
+    'axios',
+    'bbc-news',
+    'bleacher-report',
+    'bloomberg',
+    'business-insider',
+    'buzzfeed',
+    'cbc-news',
+    'cbs-news',
+    'cnbc',
+    'cnn',
+    'entertainment-weekly',
+    'espn',
+    'fortune',
+    'fox-news',
+    'google-news',
+    'nbc-news',
+    'the-washington-post',
+    'fox-sports',
+    'hacker-news',
+    'ign',
+    'mashable',
+    'msnbc',
+    'mtv-news',
+    'national-geographic',
+    'new-scientist',
+    'newsweek',
+    'nfl-news',
+    'reddit-r-all',
+    'techcrunch',
+    'talksport',
+    'techradar',
+    'the-american-conservative',
+    'the-globe-and-mail',
+    'the-hill',
+    'the-huffington-post',
+    'the-verge',
+    'the-wall-street-journal',
+    'the-washington-post',
+    'the-washington-times',
+    'time',
+    'usa-today',
+    'vice-news',
+    'wired',
+  ];
+
+  return await axios.get('https://newsapi.org/v2/top-headlines', {
     params: {
       apiKey: process.env.REACT_APP_NEWS_API_TOKEN,
       from: moment().format('YYYY-MM-DD'),
@@ -27,5 +76,7 @@ export const getNews = async ({ page = 1 }) =>
       pageSize: 12,
       q: 'COVID',
       sortBy: 'publishedAt',
+      sources: sources.join(','),
     },
   });
+};
