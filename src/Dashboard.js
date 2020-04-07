85dfb97f4fea9296f6bef1b715fe6b37d4a0dfb5
Chris Valmonte <jchrisvalmonte@gmail.com>
2020-04-07 10:01:52 -0700

    revert: Hide overview numbers grid for now

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import moment from 'moment';
-import numeral from 'numeral';
+// import numeral from 'numeral';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import Fab from '@material-ui/core/Fab';
@@ -18,7 +18,7 @@ import grey from '@material-ui/core/colors/grey';
 import red from '@material-ui/core/colors/red';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import yellow from '@material-ui/core/colors/yellow';
-import CountUp from 'react-countup';
+// import CountUp from 'react-countup';
 import { DiscreteColorLegend } from 'react-vis';
 import { Waypoint } from 'react-waypoint';
 
@@ -264,15 +264,16 @@ export default function Dashboard({ totals }) {
                   />
                 </div>
                 <HistoryChartFilters {...dateFilters} />
-
+                {/* 
+                TODO: Add back once I figure out a better information architecture
                 <Grid className={classes.numbersGrid} container xs={12}>
-                  <Grid className={classes.numberContainer} item xs={6} lg={4}>
+                  <Grid className={classes.numberContainer} item xs={6} md={4}>
                     <DashboardNumber caption="Countries affected">
                       {totals.affectedCountries}
                     </DashboardNumber>
                   </Grid>
 
-                  <Grid className={classes.numberContainer} item xs={6} lg={4}>
+                  <Grid className={classes.numberContainer} item xs={6} md={4}>
                     <DashboardNumber
                       caption="Mortality rate"
                       decimals={2}
@@ -282,7 +283,7 @@ export default function Dashboard({ totals }) {
                     </DashboardNumber>
                   </Grid>
 
-                  <Grid className={classes.numberContainer} item xs={12} lg={4}>
+                  <Grid className={classes.numberContainer} item xs={12} md={4}>
                     <DashboardNumber
                       caption="Tests administered"
                       decimals={2}
@@ -291,7 +292,7 @@ export default function Dashboard({ totals }) {
                       {totals.tests}
                     </DashboardNumber>
                   </Grid>
-                </Grid>
+                </Grid> */}
 
                 {/* Country overview table */}
                 <DataTable
@@ -393,43 +394,43 @@ function DashboardHeading({ children }) {
   );
 }
 
-function DashboardNumber({
-  caption,
-  children: number,
-  decimals = 0,
-  formattingFn = undefined,
-}) {
-  const classes = useDashboardStyles();
+// function DashboardNumber({
+//   caption,
+//   children: number,
+//   decimals = 0,
+//   formattingFn = undefined,
+// }) {
+//   const classes = useDashboardStyles();
 
-  const [counterEnd, setCounterEnd] = useState(0);
-  const [didCount, setDidCount] = useState(false);
+//   const [counterEnd, setCounterEnd] = useState(0);
+//   const [didCount, setDidCount] = useState(false);
 
-  // Update count if number changes and count animation was already seen
-  useEffect(() => {
-    if (didCount && counterEnd !== number) setCounterEnd(number);
-  }, [number]); // eslint-disable-line
+//   // Update count if number changes and count animation was already seen
+//   useEffect(() => {
+//     if (didCount && counterEnd !== number) setCounterEnd(number);
+//   }, [number]); // eslint-disable-line
 
-  const _startCounter = () => {
-    if (didCount) return;
+//   const _startCounter = () => {
+//     if (didCount) return;
 
-    setCounterEnd(number);
-    setDidCount(true);
-  };
+//     setCounterEnd(number);
+//     setDidCount(true);
+//   };
 
-  return (
-    <>
-      <Waypoint onEnter={_startCounter} />
-      <Typography align="center" component="h3" variant="h4">
-        <CountUp
-          decimals={decimals}
-          end={counterEnd}
-          formattingFn={formattingFn}
-          start={0}
-        />
-      </Typography>
-      <Typography align="center" component="p" variant="body1">
-        {caption}
-      </Typography>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Waypoint onEnter={_startCounter} />
+//       <Typography align="center" component="h3" variant="h4">
+//         <CountUp
+//           decimals={decimals}
+//           end={counterEnd}
+//           formattingFn={formattingFn}
+//           start={0}
+//         />
+//       </Typography>
+//       <Typography align="center" component="p" variant="body1">
+//         {caption}
+//       </Typography>
+//     </>
+//   );
+// }
